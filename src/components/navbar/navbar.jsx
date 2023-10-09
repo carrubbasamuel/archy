728366c65e2c75ff17cf100747a767cd1418b337
Samuel Carrubba <97916309+carrubbasamuel@users.noreply.github.com>
2023-10-09 17:48:51 +0200

    add features

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,23 +1,39 @@
+import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import logoLight from '../../asset/logo-light.png'
 import Logo from '../../asset/logo.png'
 import './style.css'
 
 
 export default function NavBar() {
     const location = useLocation()
+    const [scrolled, setScrolled] = useState(false)
 
     const isHome = location.pathname === '/'
     const isAbout = location.pathname === '/about'
     const isContact = location.pathname === '/contact'
 
+    useEffect(() => {
+        const handleScroll = () => {
+            const offset = window.scrollY
+            if (offset > 100) {
+                setScrolled(true)
+            } else {
+                setScrolled(false)
+            }
+        }
+        window.addEventListener('scroll', handleScroll)
+    }, [])
+
+    
 
     return (
-        <div id="navbar">
+        <div id="navbar" className={scrolled ? 'nav-scroll': ''}>
             <div>
-                <div className="navbar">
+                <div className='navbar'>
                     <div className='d-flex justify-content-between w-100 m-4'>
                         <div className="logo"> 
-                            <img src={Logo} alt="logo"  width={120}/>
+                            <img src={scrolled ? Logo : logoLight} alt="logo"  width={120}/>
                         </div>
                         <div className="menu">
                             <ul>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,51 @@
-import CarouselHome from '../components/carousel-home/carousel-home';
+import { Col, Row } from 'react-bootstrap';
+import { FiPlus } from 'react-icons/fi';
+import { Fade } from 'react-reveal';
 import LayoutPage from '../Layout/layout_page';
+import CarouselHome from '../components/carousel-home/carousel-home';
+import './style/home.css';
+import FirstSection from '../components/first-section-home/firstSection';
 
-export default function Home(){
-    return(
+
+export default function Home() {
+
+    return (
         <LayoutPage>
-        <div id="home">
-            <CarouselHome />
-        </div>
+            <div id="home">
+                <CarouselHome />
+                <main>
+                    <div className='first-section' >
+
+                        <FirstSection />
+                        
+
+                        {/* Pro */}
+                        <Row className='pro'>
+                            <Col>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} /> <p className='m-0 ms-2'>Feasibility Studies</p> </div>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} />  <p className='m-0 ms-2'>Conceptual Design</p></div>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} />  <p className='m-0 ms-2'>Design Development</p></div>
+                            </Col>
+                            <Col>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} />  <p className='m-0 ms-2'>Permitting and Approvals</p></div>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} />  <p className='m-0 ms-2'>Construction Documents</p></div>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} />  <p className='m-0 ms-2'>Bidding and Negotiation</p></div>
+                            </Col>
+                            <Col>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} />  <p className='m-0 ms-2'>Construction Administration</p></div>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} />  <p className='m-0 ms-2'>Post-Occupancy Evaluation</p></div>
+                                <div className='d-flex align-items-center justify-content-start mt-2'> <FiPlus size={24} />  <p className='m-0 ms-2'>Project Management</p></div>
+                            </Col>
+                        </Row>
+
+
+                    </div>
+                </main>
+
+            </div>
         </LayoutPage>
-    )
+    );
 }
+
+
+
